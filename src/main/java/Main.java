c88db9a67e0c9592dfc1567d6e1eedd2486757fd
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-01 11:46:45 +0100

    Run method parsing .csv files using OpenCSV parser in main method and print parsed objects to console

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,10 @@ public class Main {
   public static void main(String[] args) {
     ParserCsv parserCsv = new ParserCsv();
     ParserXml parserXml = new ParserXml();
+    ParserOpenCsv parserOpenCsv = new ParserOpenCsv();
 
     parserCsv.parse("requests_test_file.csv");
+    parserOpenCsv.parse("requests_test_file.csv");
     parserXml.parse("requests.xml");
 
     for (Request r : parserCsv.getRequestList())
@@ -12,6 +14,11 @@ public class Main {
     System.out.println();
 
     for (Request r : parserXml.getRequestList()) {
+      System.out.println(r);
+    }
+    System.out.println();
+
+    for (Request r : parserOpenCsv.getRequestList()) {
       System.out.println(r);
     }
   }
