5d361757349a135b1d389fd03b9191ce6524bd47
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-03 20:46:43 +0100

    Add ReportGenerator class which creates desired reports

---
+++ src/main/java/ReportGenerator.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReportGenerator {
+  List<String> report = new ArrayList<>();
+
+  List<String> generateReport(String input) {
+
+    return report;
+  }
+
+}
