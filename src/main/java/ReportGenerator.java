f23c1453d567c645225bf1dda42260fa008d39b5
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-04 14:35:10 +0100

    Add methods to return total orders number by client id and total value of orders

--- src/main/java/ReportGenerator.java
+++ src/main/java/ReportGenerator.java
@@ -1,18 +1,31 @@
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ReportGenerator {
   private List<String> report = new ArrayList<>();
+  Scanner scanner = new Scanner(System.in);
 
   List<String> generateReport(String input) throws SQLException {
-    //case input -> run method for desired report
 
     switch (input) {
       case "1": {
+        report.clear();
         report.add(String.valueOf(returnTotalOrdersNumber()));
+        break;
+      }
+      case "2": {
+        report.clear();
+        report.add(String.valueOf(returnTotalOrdersNumberOfGivenClientId()));
+        break;
+      }
+      case "3": {
+        report.clear();
+        report.add(String.valueOf(returnTotalValueOfOrders()));
         break;
       }
     }
@@ -28,6 +41,35 @@ public class ReportGenerator {
     rs.next();
     return rs.getInt(1);
   }
+
+  private int returnTotalOrdersNumberOfGivenClientId() throws SQLException {
+    System.out.print("Give Client Id: ");
+    int clientId = scanner.nextInt();
+
+    DbHandler dbHandler = DbHandler.getInstance();
+    PreparedStatement ps = dbHandler.getConnection().prepareStatement("SELECT COUNT(*) FROM REQUESTS WHERE CLIENT_ID = ?");
+    ps.setInt(1, clientId);
+    ResultSet rs = ps.executeQuery();
+    rs.next();
+    return rs.getInt(1);
+  }
+
+  private int returnTotalValueOfOrders() throws SQLException {
+    DbHandler dbHandler = DbHandler.getInstance();
+    Statement statement = dbHandler.getConnection().createStatement();
+
+    String sqlQuery = "SELECT SUM(PRICE) FROM REQUESTS;";
+    ResultSet rs = statement.executeQuery(sqlQuery);
+    rs.next();
+    return rs.getInt(1);
+  }
+
+//  total value of orders by client id
+//  list of all orders
+//  list of all orders by client id
+//  average order value
+//  average order value by client id
+
 
   public List<String> getReport() {
     return report;
