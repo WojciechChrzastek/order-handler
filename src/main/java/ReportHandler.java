32074caa6e87a6943feac8bf55b1bf25cf228aee
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-04 20:38:02 +0100

    printReportToConsole() uses ResultSet to output result

--- src/main/java/ReportHandler.java
+++ src/main/java/ReportHandler.java
@@ -1,21 +1,39 @@
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
-public class ReportHandler {
-  public ReportHandler() {
-  }
+class ReportHandler {
 
-  void printReportToConsole(List<String> report) {
-    for (String s : report) {
-      System.out.println(s);
+  void printReportToConsole(ResultSet rs) throws SQLException {
+    ResultSetMetaData rsMetaData = rs.getMetaData();
+    if (rs.getMetaData().getColumnCount() == 6) {
+      System.out.format("%3s %9s %10s %5s %8s %6s",
+              rsMetaData.getColumnLabel(1),
+              rsMetaData.getColumnLabel(2),
+              rsMetaData.getColumnLabel(3),
+              rsMetaData.getColumnLabel(4),
+              rsMetaData.getColumnLabel(5),
+              rsMetaData.getColumnLabel(6));
+
+      System.out.println();
+
+      while (rs.next()) {
+        int id = rs.getInt("id");
+        int client_id = rs.getInt("client_id");
+        int request_id = rs.getInt("request_id");
+        String name = rs.getString("name");
+        int quantity = rs.getInt("quantity");
+        double price = rs.getInt("price");
+
+        System.out.format("%3s %9s %10s %5s %8s %6s\n", id, client_id, request_id, name, quantity, price);
+      }
+    } else {
+      System.out.println(rsMetaData.getColumnLabel(1));
+      System.out.println(rs.getInt(1));
     }
   }
 
   void saveReportToCsvFile() {
 
-  }
-
-  @Override
-  public String toString() {
-    return "ReportHandler{}";
   }
 }
