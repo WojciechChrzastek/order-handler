4b1458555b160a451a8e9995734e3445b6a75b6e
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-07 15:53:23 +0100

    Test some new features in main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException, SQLException {
 //    ParserCsv parserCsv = new ParserCsv();
 //    parserCsv.parse("requests_test_file.csv");
 //
@@ -29,35 +30,43 @@ public class Main {
 //        e.printStackTrace();
 //      }
 //    }
+//
+//    String input = "1";
+//    String input2 = "2";
+//    String input3 = "3";
+//    String input4 = "4";
+//    String input5 = "5";
+//    String input6 = "6";
+//    String input7 = "7";
+//    String input8 = "8";
+//    ReportGenerator reportGenerator = null;
+//    try {
+//      reportGenerator = new ReportGenerator();
+//    } catch (SQLException e) {
+//      e.printStackTrace();
+//    }
+//    ReportHandler reportHandler = new ReportHandler();
+//
+//    try {
+//      reportHandler.printReportToConsole(reportGenerator.generateReport(input));
+//      reportHandler.printReportToConsole(reportGenerator.generateReport(input2));
+//      reportHandler.printReportToConsole(reportGenerator.generateReport(input3));
+//      reportHandler.printReportToConsole(reportGenerator.generateReport(input4));
+//      reportHandler.printReportToConsole(reportGenerator.generateReport(input5));
+//      reportHandler.printReportToConsole(reportGenerator.generateReport(input6));
+//      reportHandler.printReportToConsole(reportGenerator.generateReport(input7));
+//      reportHandler.printReportToConsole(reportGenerator.generateReport(input8));
+//
+//
+//    } catch (SQLException e) {
+//      e.printStackTrace();
+//    }
 
-    String input = "1";
-    String input2 = "2";
-    String input3 = "3";
-    String input4 = "4";
-    String input5 = "5";
-    String input6 = "6";
-    String input7 = "7";
-    String input8 = "8";
-    ReportGenerator reportGenerator = null;
-    try {
-      reportGenerator = new ReportGenerator();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+    String input = "4";
+
+    ReportGenerator reportGenerator = new ReportGenerator();
     ReportHandler reportHandler = new ReportHandler();
-
-    try {
-      reportHandler.printReportToConsole(reportGenerator.generateReport(input));
-      reportHandler.printReportToConsole(reportGenerator.generateReport(input2));
-      reportHandler.printReportToConsole(reportGenerator.generateReport(input3));
-      reportHandler.printReportToConsole(reportGenerator.generateReport(input4));
-      reportHandler.printReportToConsole(reportGenerator.generateReport(input5));
-      reportHandler.printReportToConsole(reportGenerator.generateReport(input6));
-      reportHandler.printReportToConsole(reportGenerator.generateReport(input7));
-      reportHandler.printReportToConsole(reportGenerator.generateReport(input8));
-
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+    reportHandler.printReportToConsole(reportGenerator.generateReport(input));
+    reportHandler.saveReportToCsvFile(reportGenerator.generateReport(input), input);
   }
 }
