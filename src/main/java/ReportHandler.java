d955121ff39740cb45325d853a596b0450ccf2f0
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-07 15:49:01 +0100

    Add method saveReportToCsvFile()

--- src/main/java/ReportHandler.java
+++ src/main/java/ReportHandler.java
@@ -1,3 +1,10 @@
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -7,7 +14,7 @@ class ReportHandler {
   void printReportToConsole(ResultSet rs) throws SQLException {
     ResultSetMetaData rsMetaData = rs.getMetaData();
     if (rs.getMetaData().getColumnCount() == 6) {
-      System.out.format("%3s %9s %10s %5s %8s %6s",
+      System.out.format("%3s %9s %10s %8s %8s %6s",
               rsMetaData.getColumnLabel(1),
               rsMetaData.getColumnLabel(2),
               rsMetaData.getColumnLabel(3),
@@ -25,7 +32,7 @@ class ReportHandler {
         int quantity = rs.getInt("quantity");
         double price = rs.getInt("price");
 
-        System.out.format("%3s %9s %10s %5s %8s %6s\n", id, client_id, request_id, name, quantity, price);
+        System.out.format("%3s %9s %10s %8s %8s %6s\n", id, client_id, request_id, name, quantity, price);
       }
     } else {
       System.out.println(rsMetaData.getColumnLabel(1));
@@ -33,7 +40,28 @@ class ReportHandler {
     }
   }
 
-  void saveReportToCsvFile() {
+  void saveReportToCsvFile(ResultSet rs, String input) throws IOException, SQLException {
 
+    switch (input) {
+      case "1": {
+        input = "total orders number";
+        break;
+      }
+      default: {
+        input = ("report");
+      }
+    }
+
+    String pathname = input + ".csv";
+    File file = new File(pathname);
+    CSVWriter csvWriter = new CSVWriter(
+            new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8),
+            ',', ' ',
+            CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+            CSVWriter.DEFAULT_LINE_END
+    );
+    rs.beforeFirst();
+    csvWriter.writeAll(rs, true);
+    csvWriter.close();
   }
 }
