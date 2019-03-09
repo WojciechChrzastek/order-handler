8e7f8357f13ced06a08ea0aadc6e0b5b45fcdc3b
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-09 13:05:08 +0100

    Add methods that allow user to choose and generate desired report

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
@@ -8,30 +9,32 @@ class UserInterface {
   private String input;
   private Scanner scanner = new Scanner(System.in);
   private DbHandler dbHandler = DbHandler.getInstance();
+  private ReportGenerator reportGenerator = new ReportGenerator();
+  private ReportHandler reportHandler = new ReportHandler();
 
-  void run() throws SQLException {
+  void run() throws SQLException, IOException {
     System.out.println(SoutMessages.WELCOME_HEADER);
     dbHandler.createTable();
-    mainMenu();
+    showMainMenu();
   }
 
-  private void mainMenu() throws SQLException {
+  private void showMainMenu() throws SQLException, IOException {
     System.out.println();
     do {
       System.out.print(SoutMessages.SELECT_ACTION_MAIN);
       input = scanner.nextLine();
     } while (!input.equals("1") && !input.equals("2") && !input.equals("q"));
     if (input.equals("1")) {
-      addFileMenu();
+      showAddFileMenu();
     } else if (input.equals("2")) {
-//      generateReportMenu();
+      showGenerateReportMenu();
     } else {
-      System.exit(0);
       System.out.println(SoutMessages.GOODBYE_FOOTER);
+      System.exit(0);
     }
   }
 
-  private void addFileMenu() throws SQLException {
+  private void showAddFileMenu() throws SQLException, IOException {
     System.out.println();
     do {
       System.out.print(SoutMessages.ASK_FOR_FILE_PATH);
@@ -40,22 +43,105 @@ class UserInterface {
         parseAndAddToDatabase(parserCsv.parse(input));
       } else if (input.contains(".xml")) {
         parseAndAddToDatabase(parserXml.parse(input));
-      } else if (input.contains("q")) {
-        mainMenu();
+      } else if (input.equals("q")) {
+        showMainMenu();
       } else {
         System.out.println(SoutMessages.FILE_NOT_FOUND);
         System.out.println();
       }
-    } while (!input.contains(".csv") || !input.contains("xml") || !input.contains("q"));
+    } while (!input.contains(".csv") && !input.contains("xml") && !input.equals("q"));
   }
 
-  private void parseAndAddToDatabase(List<Request> requestList) throws SQLException {
+  private void parseAndAddToDatabase(List<Request> requestList) throws SQLException, IOException {
     System.out.println(SoutMessages.PARSE_SUCCESS);
     for (Request r : requestList) {
       dbHandler.insert(r);
     }
     System.out.println(SoutMessages.ADD_FILE_DATA_SUCCESS);
-    mainMenu();
+    showMainMenu();
+  }
+
+  private void showGenerateReportMenu() throws SQLException, IOException {
+    System.out.println();
+    do {
+      System.out.print(SoutMessages.SELECT_REPORT_TYPE);
+      input = scanner.nextLine();
+    } while (!input.equals("1") && !input.equals("2") && !input.equals("q"));
+    if (input.equals("1")) {
+      showSoutReportMenu();
+    } else if (input.equals("2")) {
+      showCsvReportMenu();
+    } else {
+      showMainMenu();
+    }
+  }
+
+  private void showSoutReportMenu() throws SQLException, IOException {
+    System.out.println();
+    do {
+      System.out.print(SoutMessages.SELECT_SOUT_REPORT);
+      input = scanner.nextLine();
+    } while (!input.equals("1") && !input.equals("2") && !input.equals("3") &&
+            !input.equals("4") && !input.equals("5") && !input.equals("6") &&
+            !input.equals("7") && !input.equals("8") && !input.equals("q"));
+    if (!input.equals("q")) {
+      reportHandler.printReportToConsole(reportGenerator.generateReport(input));
+      do {
+        System.out.println();
+        System.out.print(SoutMessages.ASK_FOR_ANOTHER_SOUT_REPORT);
+        input = scanner.nextLine();
+      } while (!input.equals("1") && !input.equals("2") && !input.equals("3") && !input.equals("q"));
+      switch (input) {
+        case "1":
+          showSoutReportMenu();
+          break;
+        case "2":
+          showGenerateReportMenu();
+          break;
+        case "3":
+          showMainMenu();
+          break;
+        default:
+          System.out.println(SoutMessages.GOODBYE_FOOTER);
+          System.exit(0);
+      }
+    } else {
+      showGenerateReportMenu();
+    }
+  }
+
+  private void showCsvReportMenu() throws SQLException, IOException {
+    System.out.println();
+    do {
+      System.out.print(SoutMessages.SELECT_CSV_REPORT);
+      input = scanner.nextLine();
+    } while (!input.equals("1") && !input.equals("q"));
+    System.out.println();
+    if (input.equals("1")) {
+      reportHandler.saveReportToCsvFile(reportGenerator.generateReport(input), input);
+      System.out.println(SoutMessages.CSV_REPORT_GENERATION_SUCCESS);
+      do {
+        System.out.println();
+        System.out.print(SoutMessages.ASK_FOR_ANOTHER_CSV_REPORT);
+        input = scanner.nextLine();
+      } while (!input.equals("1") && !input.equals("2") && !input.equals("3") && !input.equals("q"));
+      switch (input) {
+        case "1":
+          showCsvReportMenu();
+          break;
+        case "2":
+          showGenerateReportMenu();
+          break;
+        case "3":
+          showMainMenu();
+          break;
+        default:
+          System.out.println(SoutMessages.GOODBYE_FOOTER);
+          System.exit(0);
+      }
+    } else {
+      showGenerateReportMenu();
+    }
   }
 
   UserInterface() throws SQLException {
