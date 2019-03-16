2999ecee5c3220f7a1aefb547b48db9d353e360f
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-16 17:54:54 +0100

    Add and implement validate method to ParserCsv

--- src/main/java/ParserCsv.java
+++ src/main/java/ParserCsv.java
@@ -7,9 +7,16 @@ import java.util.List;
 
 class ParserCsv implements Parser {
   private List<Request> requestsList = null;
+  private int lineNumber = 0;
+  private FileValidator fileValidator = new FileValidator();
 
   public List<Request> getRequestsList() {
     return requestsList;
+  }
+
+  @Override
+  public boolean validate (String line, int lineNumber) {
+    return FileValidator.validate(line, lineNumber);
   }
 
   @Override
@@ -21,14 +28,17 @@ class ParserCsv implements Parser {
     try (BufferedReader br = new BufferedReader(new FileReader(file))) {
       br.readLine();
       while ((line = br.readLine()) != null) {
-        requestsArray = line.split(",");
-        Request request = new Request();
-        request.setClientId(requestsArray[0]);
-        request.setRequestId(Long.parseLong(requestsArray[1]));
-        request.setName(requestsArray[2]);
-        request.setQuantity(Integer.parseInt(requestsArray[3]));
-        request.setPrice(new BigDecimal(requestsArray[4]));
-        requestsList.add(request);
+        lineNumber++;
+        if (validate(line, lineNumber)) {
+          requestsArray = line.split(",");
+          Request request = new Request();
+          request.setClientId(requestsArray[0]);
+          request.setRequestId(Long.parseLong(requestsArray[1]));
+          request.setName(requestsArray[2]);
+          request.setQuantity(Integer.parseInt(requestsArray[3]));
+          request.setPrice(new BigDecimal(requestsArray[4]));
+          requestsList.add(request);
+        }
       }
     } catch (IOException e) {
       System.out.println("File not found");
