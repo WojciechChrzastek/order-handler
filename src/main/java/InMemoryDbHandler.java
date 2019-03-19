a701964e27921df0fb62d39cf60d4724c4ae2378
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-19 11:40:15 +0100

    Implement createTable, setDatabase, createTable, insert and returnListOfAllOrder usung InMemoryDb in existing project

--- src/main/java/InMemoryDbHandler.java
+++ src/main/java/InMemoryDbHandler.java
@@ -5,14 +5,15 @@ import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ColumnListHandler;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 class InMemoryDbHandler {
   private Connection conn;
   private QueryRunner qr = new QueryRunner();
+
+  private String sqlQuery;
+  private ResultSet rs = null;
 
   void setDatabase() throws SQLException, ManagedProcessException {
     DBConfigurationBuilder config = DBConfigurationBuilder.newBuilder();
@@ -20,24 +21,64 @@ class InMemoryDbHandler {
     config.addArg("--default-time-zone=+0:00");
     DB db = DB.newEmbeddedDB(config.build());
     db.start();
-    String dbName = "sampleInMemoryDataBase";
+    String dbName = "sampleInMemoryDatabase";
     db.createDB(dbName);
     conn = DriverManager.getConnection(config.getURL(dbName), "root", "");
   }
 
-//  Connection getConnection() {
-//    return conn;
-//  }
+  void createTable() throws Exception {
+    String createTableQuery = "CREATE TABLE IF NOT EXISTS REQUESTS " +
+            "(" +
+            "ID SERIAL PRIMARY KEY, " +
+            "CLIENT_ID CHAR(6), " +
+            "REQUEST_ID BIGINT UNSIGNED, " +
+            "NAME VARCHAR(255) CHARSET utf8, " +
+            "QUANTITY INT UNSIGNED, " +
+            "PRICE DECIMAL (19,2) UNSIGNED " +
+            ")";
+    qr.update(conn, createTableQuery);
+  }
 
-  void test() throws Exception {
+  void insert() throws SQLException {
 
-    qr.update(conn, "CREATE TABLE hello(world VARCHAR(100))");
-    qr.update(conn, "INSERT INTO hello VALUES ('Hello, world')");
-    List<String> results = qr.query(conn, "SELECT * FROM hello", new ColumnListHandler<String>());
+//    String clientId = request.getClientId();
+//    long requestId = request.getRequestId();
+//    String name = request.getName();
+//    int quantity = request.getQuantity();
+//    BigDecimal price = request.getPrice();
+
+    PreparedStatement ps = conn.prepareStatement(
+            "INSERT INTO REQUESTS" +
+                    " (CLIENT_ID, REQUEST_ID, NAME, QUANTITY, PRICE)" +
+                    " VALUES (3, 25, 'blah', 341, 15.62)");
+
+//    ps.setString(1, clientId);
+//    ps.setLong(2, requestId);
+//    ps.setString(3, name);
+//    ps.setInt(4, quantity);
+//    ps.setBigDecimal(5, price);
+
+    ps.executeUpdate();
+  }
+
+  void select() throws SQLException {
+    List<String> results = qr.query(conn, "SELECT * FROM REQUESTS", new ColumnListHandler<>());
 
     System.out.println(results.size());
     System.out.println(results.get(0));
 
+
+  }
+
+  void closeDb() {
     DbUtils.closeQuietly(conn);
+  }
+
+  ResultSet returnListOfAllOrders() throws SQLException {
+    sqlQuery = "SELECT * FROM REQUESTS";
+    Statement statement = conn.createStatement();
+    rs = statement.executeQuery(sqlQuery);
+    rs.beforeFirst();
+    return rs;
   }
 }
