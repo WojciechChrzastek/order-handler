4a0d234cfe5cd2c03a93766fbdd179b3d69307a4
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-22 14:38:33 +0100

    Clean-up the class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,5 @@ public class Main {
   public static void main(String[] args) throws Exception {
     UserService userService = new UserService();
     userService.run();
-
-
-//    InMemoryDbHandler inMemoryDbHandler = new InMemoryDbHandler();
-//    inMemoryDbHandler.setDatabase();
-//    inMemoryDbHandler.createTable();
-//    inMemoryDbHandler.showTables();
-//    inMemoryDbHandler.closeDb();
-
-//    ReportHandler reportHandler = new ReportHandler();
-//    reportHandler.printReportToConsole(inMemoryDbHandler.returnListOfAllOrders());
-
   }
 }
