a7a3e89d8b8a54d7b57060208bffc31dad046a10
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-25 20:43:11 +0100

    Shorten ReportGenerator class changing private methods into if-else block

--- src/main/java/ReportGenerator.java
+++ src/main/java/ReportGenerator.java
@@ -10,112 +10,65 @@ class ReportGenerator {
   private int clientId;
 
   ResultSet generateReport(String input, Connection conn) throws SQLException {
+    int report = Integer.parseInt(input);
 
-    switch (input) {
-      case "1": {
-        rs = returnTotalOrdersNumber(conn);
-        break;
+    if (report == 1 | (report != 5 && report != 6)) {
+      if (report == 1) {
+        rs = conn.createStatement().executeQuery(SqlQueries.RETURN_TOTAL_ORDERS_NUMBER);
+      } else {
+        switch (report) {
+          case 2: {
+            obtainClientId();
+            PreparedStatement ps = conn.prepareStatement(SqlQueries.RETURN_TOTAL_ORDERS_NUMBER_OF_GIVEN_CLIENT_ID);
+            ps.setString(1, "TOTAL_ORDERS_NUMBER_FOR_CLIENT_ID_NO_" + clientId);
+            ps.setInt(2, clientId);
+            rs = ps.executeQuery();
+            break;
+          }
+          case 3: {
+            PreparedStatement ps = conn.prepareStatement(SqlQueries.RETURN_TOTAL_VALUE_OF_ORDERS);
+            rs = ps.executeQuery();
+            break;
+          }
+          case 4: {
+            obtainClientId();
+            PreparedStatement ps = conn.prepareStatement(SqlQueries.RETURN_TOTAL_VALUE_OF_ORDERS_OF_GIVEN_CLIENT_ID);
+            ps.setString(1, "TOTAL_VALUE_OF_ORDERS_FOR_CLIENT_ID_NO_" + clientId);
+            ps.setInt(2, clientId);
+            rs = ps.executeQuery();
+            break;
+          }
+          case 7: {
+            rs = conn.createStatement().executeQuery(SqlQueries.RETURN_AVERAGE_ORDER_VALUE);
+            break;
+          }
+          case 8: {
+            obtainClientId();
+            PreparedStatement ps = conn.prepareStatement(SqlQueries.RETURN_AVERAGE_ORDER_VALUE_OF_GIVEN_CLIENT_ID);
+            ps.setString(1, "AVERAGE_ORDER_VALUE_FOR_CLIENT_ID_NO_" + clientId);
+            ps.setInt(2, clientId);
+            rs = ps.executeQuery();
+            break;
+          }
+        }
       }
-      case "2": {
-        rs = returnTotalOrdersNumberOfGivenClientId(conn);
-        break;
+      rs.next();
+    } else {
+      if (report == 5) {
+        rs = conn.createStatement().executeQuery(SqlQueries.RETURN_LIST_OF_ALL_ORDERS);
+      } else {
+        obtainClientId();
+        PreparedStatement ps = conn.prepareStatement(SqlQueries.RETURN_LIST_OF_ALL_ORDERS_OF_GIVEN_CLIENT_ID);
+        ps.setInt(1, clientId);
+        rs = ps.executeQuery();
       }
-      case "3": {
-        rs = returnTotalValueOfOrders(conn);
-        break;
-      }
-      case "4": {
-        rs = returnTotalValueOfOrdersOfGivenClientId(conn);
-        break;
-      }
-      case "5": {
-        rs = returnListOfAllOrders(conn);
-        break;
-      }
-      case "6": {
-        rs = returnListOfAllOrdersOfGivenClientId(conn);
-        break;
-      }
-      case "7": {
-        rs = returnAverageOrderValue(conn);
-        break;
-      }
-      case "8": {
-        rs = returnAverageOrderValueOfGivenClientId(conn);
-        break;
-      }
-      default: {
-        System.out.println("Wrong input");
-      }
+      rs.beforeFirst();
     }
     return rs;
   }
 
-  private ResultSet returnTotalOrdersNumber(Connection conn) throws SQLException {
-    rs = conn.createStatement().executeQuery(SqlQueries.RETURN_TOTAL_ORDERS_NUMBER);
-    rs.next();
-    return rs;
-  }
-
-  private ResultSet returnTotalOrdersNumberOfGivenClientId(Connection conn) throws SQLException {
+  private void obtainClientId() {
     System.out.println(SoutMessages.GIVE_CLIENT_ID);
     clientId = scanner.nextInt();
-    PreparedStatement ps = conn.prepareStatement(SqlQueries.RETURN_TOTAL_ORDERS_NUMBER_OF_GIVEN_CLIENT_ID);
-    ps.setString(1, "TOTAL_ORDERS_NUMBER_FOR_CLIENT_ID_NO_" + clientId);
-    ps.setInt(2, clientId);
-    rs = ps.executeQuery();
-    rs.next();
-    return rs;
-  }
-
-  private ResultSet returnTotalValueOfOrders(Connection conn) throws SQLException {
-    PreparedStatement ps = conn.prepareStatement(SqlQueries.RETURN_TOTAL_VALUE_OF_ORDERS);
-    rs = ps.executeQuery();
-    rs.next();
-    return rs;
-  }
-
-  private ResultSet returnTotalValueOfOrdersOfGivenClientId(Connection conn) throws SQLException {
-    System.out.println(SoutMessages.GIVE_CLIENT_ID);
-    clientId = scanner.nextInt();
-    PreparedStatement ps = conn.prepareStatement(SqlQueries.RETURN_TOTAL_VALUE_OF_ORDERS_OF_GIVEN_CLIENT_ID);
-    ps.setString(1, "TOTAL_VALUE_OF_ORDERS_FOR_CLIENT_ID_NO_" + clientId);
-    ps.setInt(2, clientId);
-    rs = ps.executeQuery();
-    rs.next();
-    return rs;
-  }
-
-  private ResultSet returnListOfAllOrders(Connection conn) throws SQLException {
-    rs = conn.createStatement().executeQuery(SqlQueries.RETURN_LIST_OF_ALL_ORDERS);
-    rs.beforeFirst();
-    return rs;
-  }
-
-  private ResultSet returnListOfAllOrdersOfGivenClientId(Connection conn) throws SQLException {
-    System.out.println(SoutMessages.GIVE_CLIENT_ID);
-    clientId = scanner.nextInt();
-    PreparedStatement ps = conn.prepareStatement(SqlQueries.RETURN_LIST_OF_ALL_ORDERS_OF_GIVEN_CLIENT_ID);
-    ps.setInt(1, clientId);
-    rs = ps.executeQuery();
-    rs.beforeFirst();
-    return rs;
-  }
-
-  private ResultSet returnAverageOrderValue(Connection conn) throws SQLException {
-    rs = conn.createStatement().executeQuery(SqlQueries.RETURN_AVERAGE_ORDER_VALUE);
-    rs.next();
-    return rs;
-  }
-
-  private ResultSet returnAverageOrderValueOfGivenClientId(Connection conn) throws SQLException {
-    System.out.println(SoutMessages.GIVE_CLIENT_ID);
-    clientId = scanner.nextInt();
-    PreparedStatement ps = conn.prepareStatement(SqlQueries.RETURN_AVERAGE_ORDER_VALUE_OF_GIVEN_CLIENT_ID);
-    ps.setString(1, "AVERAGE_ORDER_VALUE_FOR_CLIENT_ID_NO_" + clientId);
-    ps.setInt(2, clientId);
-    rs = ps.executeQuery();
-    rs.next();
-    return rs;
   }
 }
